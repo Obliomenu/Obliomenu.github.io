88c7231370be26ff06368589e30dfe070fc82a02
Obliomenu <robertoqu8@gmail.com>
2022-03-26 16:37:19 +0100

    index+style+script: miglioramenti

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ let menuHamb = document.querySelector('.cs_bar');
 let menu = document.querySelector('.hamburger-menu');
 
 let menuClose = document.querySelector('.close');
-console.log(menuHamb, menu, menuClose);
+
+let menuLink = document.querySelectorAll('.cs_menu_item')
+console.log(menuHamb, menu, menuClose, menuLink);
 
 menuHamb.addEventListener('click', function(){
     menu.classList.add('cs_active');
     console.log(menu);
     menu.classList.add('hamburger-show');
 });
+
+menuLink.forEach(item => {
+    item.addEventListener('click', event => {
+        menu.classList.remove('cs_active');
+    })
+})
 
 menuClose.addEventListener('click', function(){
     menu.classList.remove('cs_active');
